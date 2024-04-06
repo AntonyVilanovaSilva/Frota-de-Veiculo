de720286265c212318847ef436c64b9665158195
AntonyVilanovaSilva <115712032+AntonyVilanovaSilva@users.noreply.github.com>
2024-04-06 11:52:25 -0300

    06/04

--- Frota de Veículos/SQLQuery1.sql
+++ Frota de Veículos/SQLQuery1.sql
@@ -12,8 +12,10 @@ VeiMarca varchar(100) null,
 CombId int not null FOREIGN KEY REFERENCES TipoCombustivel(CombId)
 )
 
+Insert into Veiculo (VeiPlaca,VeiCor,VeiMarca,CombId) values ('JQB-7519','Vermelho','Mercedes',1)
 Select * from Veiculo
 DROP TABLE Veiculo
+
 
 Create Table Motorista(
 MotId int identity(1,1) primary key,
@@ -22,7 +24,10 @@ Idade varchar(100) null,
 VeId int not null FOREIGN KEY REFERENCES Veiculo(VeId)
 )
 
+Insert into Motorista (Nome,Idade,VeId) values ('Antony','22',4)
+
 Select * from Motorista
+
 
 Create Table Posto(
 PoId int identity(1,1) primary key,
@@ -31,6 +36,7 @@ Localizacao varchar(100) null,
 OrdSerId int not null FOREIGN KEY REFERENCES OrdemServico(OrdSerId)
 )
 
+Insert into Posto (Nome,Localizacao,OrdSerId) values ('Posto Estanciano','Estancia',4)
 Select * from Posto
 
 
@@ -40,7 +46,7 @@ ValorLitro float null,
 Nome varchar(100) not null,
 )
 
-select * from TipoCombustivel
+Insert into TipoCombustivel (ValorLitro,Nome) values ('5.60','Diesel')
 
 Create Table OrdemServico(
 OrdSerId int identity(1,1) primary key,
@@ -49,3 +55,6 @@ QuantidadeCombustivel float,
 MotId int not null FOREIGN KEY REFERENCES Motorista(MotId),
 CombId int not null FOREIGN KEY REFERENCES TipoCombustivel(CombId)
 )
+
+Insert into OrdemServico (Data,QuantidadeCombustivel,MotId,CombId) values ('04-04-2024','1500',5,1)
+select * from OrdemServico
